6c69e4949b37f657e7fab66132159c9e5459f89c
Renan Roberto <renanroberto1@gmail.com>
2018-12-31 21:29:24 -0200

    read and write on sqlite

--- src/Server.hs
+++ src/Server.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings, DataKinds, GADTs,
-   TypeOperators, DeriveGeneric, DeriveAnyClass #-}
+    TypeOperators, DeriveGeneric, DeriveAnyClass #-}
 
 module Server (webAppEntry) where
 
 import Data.List
 import Data.Time
 import Data.Aeson
+import Control.Monad.IO.Class
 import GHC.Generics (Generic)
 import Servant hiding (Unauthorized)
 import Servant.Checked.Exceptions
@@ -51,18 +52,21 @@ instance FromHttpApiData LessonSort where
 
 
 data Lesson = Lesson
-  { lesson_id :: Int
+  { lesson_id :: Maybe Int
   , lesson_title :: String
   , lesson_date :: Day
   } deriving (Show, Generic)
 
+dropPrefix :: String -> [a] -> [a]
+dropPrefix str = drop (length str)
+
 instance ToJSON Lesson where
   toJSON = genericToJSON defaultOptions
-    { fieldLabelModifier = drop 7 } -- length "lesson_" == 7
+    { fieldLabelModifier = dropPrefix "lesson_" }
 
 instance FromJSON Lesson where
   parseJSON = genericParseJSON defaultOptions
-    { fieldLabelModifier = drop 7 }
+    { fieldLabelModifier = dropPrefix "lesson_" }
 
 
 instance ToRow Lesson where
@@ -72,23 +76,42 @@ instance FromRow Lesson where
   fromRow = Lesson <$> field <*> field <*> field
 
 
-{-- Real DB --}
+{-- Real DB example --}
 database :: IO ()
 database = do
   conn <- open "crud.db"
-  execute_ conn "create table if not exists lessons (id int primary key, title varchar(50), date varchar(10))"
-  execute conn "insert into lessons values (?, ?, ?)" (2 :: Int, "análise" :: String, "2019-12-12" :: String) 
+  execute_ conn "create table if not exists lessons (id integer primary key, title varchar(50), date date)"
+  execute conn "insert into lessons (title, date) values (?, ?)" ("análise" :: String, "2019-12-12" :: String) 
+  execute conn "insert into lessons (title, date) values (?, ?)" ("geometria" :: String, "2018-12-12" :: String) 
   lesson <- query_ conn "select id, title, date from lessons" :: IO [Lesson]
   close conn
   print lesson
-{-- Real DB --}
 
+
+listLessonsDB :: IO [Lesson]
+listLessonsDB = do
+  conn <- open "crud.db"
+  lessons <- query_ conn "select * from lessons limit 100" :: IO [Lesson]
+  close conn
+  return lessons
+
+insertLessonDB :: Lesson -> IO Lesson
+insertLessonDB lesson = do
+  conn <- open "crud.db"
+  execute conn "insert into lessons (title, date) values (?, ?)"
+    ( lesson_title lesson
+    , lesson_date lesson
+    )
+  [savedLesson] <- query_ conn "select * from lessons order by id desc limit 1" :: IO [Lesson]
+  close conn
+  return savedLesson
+{-- Real DB example --}
 
 {-- Fake DB --}
 lessons :: [Lesson]
 lessons =
-  [ Lesson 1 "Mecanica Quantica" (fromGregorian 2018 12 24)
-  , Lesson 2 "Teoria dos Numeros" (fromGregorian 2018 11 26)
+  [ Lesson (Just 1) "Mecanica Quantica" (fromGregorian 2018 12 24)
+  , Lesson (Just 2) "Teoria dos Numeros" (fromGregorian 2018 11 26)
   ]
 
 endpoints :: [Endpoint]
@@ -103,31 +126,32 @@ root :: Handler [Endpoint]
 root = return endpoints
 
 
-sortLesson :: Ord a => (Lesson -> a) -> [Lesson] -> [Lesson]
-sortLesson prop = sortBy compareProps
-  where compareProps :: Lesson -> Lesson -> Ordering
-        compareProps x y = compare (prop x) (prop y)
-
+-- sortLesson :: Ord a => (Lesson -> a) -> [Lesson] -> [Lesson]
+-- sortLesson prop = sortBy compareProps
+--   where compareProps :: Lesson -> Lesson -> Ordering
+--         compareProps x y = compare (prop x) (prop y)
+-- 
+-- 
+-- listLessons :: Maybe LessonSort -> Handler [Lesson]
+-- listLessons (Just LessonByTitle) = return (sortLesson lesson_title lessons)
+-- listLessons (Just LessonByDate) = return (sortLesson lesson_date lessons)
+-- listLessons Nothing = return lessons
 
 listLessons :: Maybe LessonSort -> Handler [Lesson]
-listLessons (Just LessonByTitle) = return (sortLesson lesson_title lessons)
-listLessons (Just LessonByDate) = return (sortLesson lesson_date lessons)
-listLessons Nothing = return lessons
-
+listLessons _ = liftIO $ listLessonsDB
 
 getLesson :: Int -> Maybe String -> Handler (Envelope '[ResponseErr] Lesson)
 getLesson _ Nothing = pureErrEnvelope Unauthorized
 getLesson lid _ =
   if lid > 0
-     then case find (\l -> lesson_id l == lid) lessons of
+     then case find (\l -> lesson_id l == Just lid) lessons of
             Nothing -> pureErrEnvelope NotFound
             Just lesson -> pureSuccEnvelope lesson
      else pureErrEnvelope BadRequest
 
 
 addLesson :: Lesson -> Handler Lesson
-addLesson lesson = return lesson -- TODO
-
+addLesson = liftIO . insertLessonDB
 
 server :: Server API
 server = root
